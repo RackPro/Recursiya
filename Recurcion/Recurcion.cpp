628b5948d3d80f2dd85981a39ce0a42bf2bc66ec
Марат <kusegenov1029@mail.ru>
2021-06-25 00:57:10 +0300

    Добавьте файлы проекта.

---
+++ Recurcion/Recurcion.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream> 
+using namespace std;
+
+unsigned long int fib(unsigned long int n)
+{
+	if (n == 1 || n == 2) return 1;
+	if (n == 0) return 0;
+	return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int a;
+	cout << "Введите кол-во циклов" << endl;
+	cin >> a;
+	for (int i = 0; i < a; i++)
+		cout << fib(i) << "" << endl;
+	cout << "Ваш результат";
+	system("pause");
+	return 0;
+}
